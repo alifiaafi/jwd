2ae37e5fe1ca5930fd074d30e7810ed9a5b2ac61
av <134401933+alifiaafi@users.noreply.github.com>
2023-08-24 11:22:12 +0700

    Add files via upload

---
+++ JWD - ALIFIA/data.php
@@ -0,0 +1,13 @@
+<?php
+$umur = ["0-10", "11-17", ">17"];
+$arrayLength = count ($umur);
+
+if ($data ["umur"] == "0-10") {
+    $type = anak-anak;
+} else if ($data ["umur"] == "11-17") {
+    $type = remaja;
+} else if ($data ["umur"] == ">17"){
+    $type = dewasa;
+}
+
+?>
